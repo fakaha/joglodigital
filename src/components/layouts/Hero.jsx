660b27f9c015d095e7205ca9fa18d475017872f7
fakaha <zulfakaha@students.amikom.ac.id>
2026-01-08 02:15:12 +0700

    delete useless class

--- src/components/layouts/Hero.jsx
+++ src/components/layouts/Hero.jsx
@@ -6,8 +6,8 @@ export const Hero = () => {
   return (
     <div className='px-4 pt-36 pb-28 text-[#fff] text-center h-svh flex flex-col justify-center items-center' style={{backgroundImage: `url(${banner})`,backgroundSize: 'cover',
     backgroundPosition: 'center',}}>
-        <h1 className='text-2xl mb-8'>Bantu Digitalisasi Bisnis Kamu</h1>
-        <h2 className='text-4xl mb-8 lead lh-base'>Mitra Terbaik dalam Pengembangan Website Bisnis Anda</h2>
+        <h1 className='text-2xl mb-8'>Digitalisasi Bisnis Anda Mudah & Cepat</h1>
+        <h2 className='text-4xl mb-8 max-w-[700px]'>Mitra Terbaik Dalam Pengembangan Website Untuk Keperluan Bisnis Anda</h2>
         <Button style={{backdropFilter: 'blur(5px)', backgroundColor: 'rgba(255,255,255,0.3)'}} link='https://wa.me/6287708899020?text=Halo Fakanet, saya mau bikin website untuk usaha saya' font=''>Hubungi Kami</Button>
     </div>
   )
